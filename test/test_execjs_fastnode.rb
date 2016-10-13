671704d47dd2d6f8de2afd730cb2c497c7e05ac8
John Hawthorn <john.hawthorn@gmail.com>
2016-10-12 22:11:21 -0700

    Rename fastnode test

--- test/test_execjs_fastnode.rb
+++ test/test_execjs_fastnode.rb
@@ -1,11 +1,7 @@
 require 'test_helper'
 
-class ExecJS::FastNodeTest < Minitest::Test
+class TestExecJSFastNode < Minitest::Test
   def test_that_it_has_a_version_number
     refute_nil ::ExecJS::FastNode::VERSION
-  end
-
-  def test_it_does_something_useful
-    assert false
   end
 end
