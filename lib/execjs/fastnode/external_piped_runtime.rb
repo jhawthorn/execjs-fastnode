169198cf585f556fcdbc577568dd801de55d9ad2
John Hawthorn <john.hawthorn@gmail.com>
2016-10-27 00:11:56 -0700

    Switch to using http over a UNIX socket
    This should be a little more robust and simplify our synchronization.
    
    Technique borrowed from Alaska
    https://github.com/mavenlink/alaska

--- lib/execjs/fastnode/external_piped_runtime.rb
+++ lib/execjs/fastnode/external_piped_runtime.rb
@@ -1,28 +1,70 @@
+require "net/http"
+require "socket"
+require "thread"
 require "tmpdir"
 require 'json'
-require "open3"
-require "thread"
 require "execjs/runtime"
 require "execjs/fastnode/command"
 
 module ExecJS
   module FastNode
     class ExternalPipedRuntime < ExecJS::Runtime
-      class VM
-        attr_reader :stdin, :stdout
+      class VMCommand
+        def initialize(socket_path, cmd, arguments)
+          @socket_path = socket_path.to_s
+          @cmd = cmd
+          @arguments = arguments
+        end
 
+        def execute
+          sock = Net::BufferedIO.new(socket)
+
+          request = Net::HTTP::Post.new("/")
+          request['Connection'] = 'close'
+          request['Content-Type'] = 'application/json'
+          request.body = contents
+          request.exec(sock, "1.1", "/")
+
+          begin
+            response = Net::HTTPResponse.read_new(sock)
+          end while response.kind_of?(Net::HTTPContinue)
+
+          response.reading_body(sock, request.response_body_permitted?) { }
+          sock.close
+
+          parse(response.body)
+        end
+
+        private
+
+        def socket
+          UNIXSocket.new(@socket_path)
+        end
+
+        def contents
+          ::JSON.generate({cmd: @cmd, args: @arguments})
+        end
+
+        def parse(body)
+          ::JSON.parse(body, create_additions: false)
+        end
+      end
+
+      class VM
         def initialize(options)
           @mutex = Mutex.new
-          @stdin = @stdout = @wait_thr = nil
+          @socket_path = nil
           @options = options
         end
 
         def started?
-          !!@stdin
+          !!@socket_path
         end
 
-        def self.finalize(stdin)
-          proc { stdin.puts('{"cmd": "exit", "arguments": [0]}') }
+        def self.finalize(socket_path)
+          proc {
+            VMCommand.new(socket_path, "exit", [0]).execute
+          }
         end
 
         def exec(context, source)
@@ -43,16 +85,27 @@ module ExecJS
 
         def start_without_synchronization
           return if started?
-          @stdin, @stdout, @wait_thr = Open3.popen2(@options[:binary], @options[:runner_path])
-          ObjectSpace.define_finalizer(self, self.class.finalize(@stdin))
+          dir = Dir.mktmpdir("execjs-fastnode-")
+          @socket_path = File.join(dir, "socket")
+          @pid = Process.spawn({"PORT" => @socket_path.to_s}, @options[:binary], @options[:runner_path])
+
+          retries = 20
+          while !File.exists?(@socket_path)
+            sleep 0.05
+            retries -= 1
+
+            if retries == 0
+              raise "Unable to start nodejs process"
+            end
+          end
+
+          ObjectSpace.define_finalizer(self, self.class.finalize(@socket_path))
         end
 
         def command(cmd, *arguments)
           @mutex.synchronize do
             start_without_synchronization
-            @stdin.puts(::JSON.generate({cmd: cmd, args: arguments}))
-            str = @stdout.gets
-            result = ::JSON.parse(str, create_additions: false)
+            VMCommand.new(@socket_path, cmd, arguments).execute
           end
         end
       end
